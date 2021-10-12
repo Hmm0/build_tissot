e651e84c0fb1213c0522215c34c53fe62a4640dd
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2021-10-12 20:51:43 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 -u https://github.com/ArrowOS -b arrow-12.0
+repo init --depth=1 -u https://github.com/ArrowOS/android_manifest -b arrow-12.0
 
 # Sync source
 repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune -c --no-clone-bundle --no-tags
@@ -8,7 +8,7 @@ git clone https://github.com/Hmm0/android_device_xiaomi_tissot -b a12 device/xia
 git clone https://github.com/Hmm0/android_device_xiaomi_msm8953-common -b a12 device/xiaomi/msm8953-common --depth=1
 git clone https://github.com/neonmicky/kernel_xiaomi_tissot -b test kernel/xiaomi/msm8953 --depth=1
 git clone https://github.com/Hmm0/vendor_xiaomi_tissot -b tissot vendor/xiaomi/tissot
-got clone https://github.com/Hmm0/vendor_xiaomi_msm8953-common -b msm8953-common vendor/xiaomi/msm8953-common
+git clone https://github.com/Hmm0/vendor_xiaomi_msm8953-common -b msm8953-common vendor/xiaomi/msm8953-common
 rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
