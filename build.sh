593d38e996540aa8ab55901977e83cd4ff1a678f
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-01-22 19:08:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
 #make bootimage -j24
-lunch aosp_tissot-userdebug
+lunch spark_tissot-userdebug
 #mka vendorimage
 #ccache -s
-m aex
+mka bacon
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
