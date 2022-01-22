085c249e9cbec9b01faf0bc3bf58abd9156583de
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-01-22 19:11:39 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,15 +1,5 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 -u https://github.com/aex-tmp/manifest.git -b 12.x
-
-# Sync source
-repo sync -j24 --prune -c --no-clone-bundle --no-tags || repo sync -j24 --prune -c --no-clone-bundle --no-tags
-git clone https://github.com/Unknownbitch07/android_device_xiaomi_tissot -b 12 device/xiaomi/tissot --depth=1
-git clone https://github.com/Unknownbitch07/android_device_xiaomi_msm8953-common -b twelve device/xiaomi/msm8953-common --depth=1
-git clone https://github.com/glassydan/kernel_xiaomi_tissot -b twelve kernel/xiaomi/msm8953 --depth=1
-git clone https://github.com/Hmm0/vendor_xiaomi_tissot -b tissot vendor/xiaomi/tissot
-git clone https://github.com/Hmm0/vendor_xiaomi_msm8953-common -b msm8953-common vendor/xiaomi/msm8953-common
-rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display
-git clone https://github.com/Unknownbitch07/R-Hals -b display-s hardware/qcom-caf/msm8996/display
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b spark .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
