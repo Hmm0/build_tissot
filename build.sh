5dc35fe45be0c191a312f53081ec664ef53606b1
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-03-22 07:22:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,12 @@
 
 # Cook
 . build/envsetup.sh
-#export CCACHE_DIR=/drone/ccache
-#export CCACHE_EXEC=$(which ccache)
-#export USE_CCACHE=1
-#lunch statix_tissot-eng
 ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-#make bootimage -j24
-lunch xdroid_tissot-userdebug
-#mka vendorimage
-#ccache -s
-make xd
+lunch palladium_tissot-userdebug
+mka palladium
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
