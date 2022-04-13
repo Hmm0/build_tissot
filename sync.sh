de3c5c72470ccfc78e8abba8ec8ae7f697f18b56
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-04-14 04:53:50 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b palladium .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
