6ec206f39f1a12784c2bdc6856281232d77c94d6
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-04-14 15:19:13 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b twelve .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b arcana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
