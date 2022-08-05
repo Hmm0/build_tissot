6124d87e9933df4f48b3b98f1459a589f334155a
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-05 17:46:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch xdroid_sunfish-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+lunch xdroid_sunfish-userdebug
 make xd
 #echo "Build Done"
 #cd out/target/product/tissot
