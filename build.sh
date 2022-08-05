9be5c88b4a0ab75239bdeea64681ae61a951bf78
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-05 20:58:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ ccache -z
 echo "Starting Cooking"
 lunch xdroid_sunfish-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_DUP_RULES := true
 make xd
 #echo "Build Done"
 #cd out/target/product/tissot
