3b9c58aa3bb3820d2e9470f5a007f89b089f329e
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-06 13:13:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ ccache -z
 echo "Starting Cooking"
 lunch xdroid_sunfish-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES := true
 make xd
 #echo "Build Done"
