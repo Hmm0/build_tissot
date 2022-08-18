e3d285210c844e9683eb8bcd9e2101aac74c39ec
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-18 16:48:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #echo "Build script"
 
 # Cook
-. build/envsetup.sh
+source build/envsetup.sh
 ccache -M 20G
 ccache -o compression=true
 ccache -z
@@ -10,7 +10,7 @@ echo "Starting Cooking"
 lunch lineage_sunfish-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES= true
+export BUILD_BROKEN_DUP_RULES=true
 m bacon
 #echo "Build Done"
 #cd out/target/product/tissot
