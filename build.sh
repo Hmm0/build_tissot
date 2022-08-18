a5b17d2a9088c63b9403a4aec35f673939a64b58
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-18 15:44:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "Starting Cooking"
 lunch lineage_sunfish-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES := true
+export BUILD_BROKEN_DUP_RULES= true
 m bacon
 #echo "Build Done"
 #cd out/target/product/tissot
