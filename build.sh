62ff313211772daf29043ae0eecc6f089de88814
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-08-20 19:36:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch lineage_sunfish-userdebug
+lunch spark_sunfish-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-m bacon
+mka bacon
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
