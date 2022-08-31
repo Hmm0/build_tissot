aad7c903fd0dcd8d7bd0a5ce23bc82feaa8b2124
Hmm0 <elprofessor936@gmail.com>
2022-08-31 10:47:06 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ ccache -z
 echo "Starting Cooking"
 lunch evolution_sunfish-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 make evolution
 #echo "Build Done"
