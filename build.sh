14e80b33b0775f553ef207c9ea6f1521d39ce3ee
Hmm0 <elprofessor936@gmail.com>
2022-08-31 06:18:13 +0000

    adapt to build evo

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch xdroid_sunfish-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
-make xd
+lunch evolution_sunfish-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+make evolution
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
