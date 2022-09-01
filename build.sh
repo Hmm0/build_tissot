f56258b1e125c04f910259b3376c7f32351dd2f9
Hmm0 <elprofessor936@gmail.com>
2022-09-01 06:15:25 +0000

    update

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch evolution_sunfish-userdebug
+lunch derp_sunfish-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-make evolution
+make derp
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
