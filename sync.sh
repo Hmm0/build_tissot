2427bf2c4d1835d73c9036fd2b68682371e6bc2f
Hmm0 <elprofessor936@gmail.com>
2022-09-03 15:47:49 +0000

    update

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b evo12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b cr12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
