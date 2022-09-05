b8db2c25b4f083f9ef07676c560d4648063f1074
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-09-05 07:42:53 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 # Initialize Repos
 echo "Initializing Manifest"
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b derp12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b xdroid12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
