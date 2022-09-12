d4cb2f940af8e91e7bd67b6b5d01a67c5065ca83
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-09-12 11:22:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ lunch xdroid_sunfish-userdebug
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-#export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 make xd
 #echo "Build Done"
 #cd out/target/product/tissot
