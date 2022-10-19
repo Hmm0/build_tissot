603b5fd3fa978502a1597f780eae93aff3b0eb1a
Hmm0 <64182873+Hmm0@users.noreply.github.com>
2022-10-19 09:09:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ ccache -M 20G
 ccache -o compression=true
 ccache -z
 echo "Starting Cooking"
-lunch xdroid_sunfish-userdebug
+lunch p404_sunfish-userdebug
+export WITH_GAPPS=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 #export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-make xd
+make bacon
 #echo "Build Done"
 #cd out/target/product/tissot
 #curl bashupload.com -T boot.img
